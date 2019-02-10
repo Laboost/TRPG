1470c9b5383e2ed411ee6148c34c1a8a7f05e24c
Unknown <omer531998@gmail.com>
2019-02-11 00:10:48 +0200

    Added Option

--- TRP/Menu.cs
+++ TRP/Menu.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +6,16 @@ namespace TRP
 {
     class Menu
     {
+
+    class Option
+    {
+        public string text;
+        public Action action;
+        
+        public Option(string text, Action action)
+        {
+            this.text = text;
+            this.action = action;
+        }
     }
 }
